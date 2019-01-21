774379d0ac82e92392655535fbe00333ecf1e710
Bruno Baradel <brunobaradel@gmail.com>
2019-01-21 18:55:42 -0200

    trigger feito

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,5 @@
+/* Verifica se a data de entrada de um veiculo no estacionamento é válida */
+
 --TRIGGER FUNCTION
 CREATE OR REPLACE FUNCTION verificaData() RETURNS trigger AS $testeTrigger$
 BEGIN
@@ -10,13 +12,34 @@ RETURN NEW;
 END;
 $testeTrigger$ LANGUAGE plpgsql;
 
-
 --TRIGGER
-
 CREATE TRIGGER verificaData 
 AFTER INSERT OR UPDATE ON cadastra
 FOR EACH ROW
 EXECUTE PROCEDURE verificaData();
 
 
-/* trigger para antes de inserir um carro, verificar se ele ja saiu do estacionamento (verificar a tabela de permanencia, se a hora de saida esta nula) */
+
+/* Antes de inserir um carro, verifica se o mesmo ja saiu do estacionamento */
+
+--TRIGGER FUNCTION
+CREATE OR REPLACE FUNCTION verificaEntrada() RETURNS trigger AS $testeTrigger$
+BEGIN
+	
+	IF NEW.placa_veiculo in (select permanencia.placa_veiculo
+					from permanencia
+					where permanencia.dataHora_saida is null)
+	THEN
+	RAISE EXCEPTION 'Veículo ainda está estacionado!';
+    END IF;
+
+RETURN NEW;
+END;
+$testeTrigger$ LANGUAGE plpgsql;
+
+--TRIGGER
+CREATE TRIGGER testeTrigger
+BEFORE INSERT OR UPDATE ON cadastra
+FOR EACH ROW EXECUTE PROCEDURE verificaEntrada();
+
+
